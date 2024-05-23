37c587f32e0e7842dc2b5194dceae1f427dbc4ca
shuyaoao <124263084+shuyaoao@users.noreply.github.com>
2024-05-23 22:23:25 +0800

    Add files via upload

--- nlp/src/NLPManager.py
+++ nlp/src/NLPManager.py
@@ -1,40 +1,80 @@
-from langchain.output_parsers import PydanticOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain_openai import OpenAI
-import os
 from typing import Dict
+# from transformers import BertForQuestionAnswering, AutoTokenizer, Trainer, BertTokenizer, BertForSequenceClassification
+import torch
+from fuzzywuzzy import process
+from transformers import pipeline
 
 class NLPManager:
     def __init__(self):
-        # Initialize the OpenAI model
-        self.model = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.0)
-
-        # Define your desired data structure.
-        class JSONObj(BaseModel):
-            target: str = Field(description="identified target")
-            heading: str = Field(description="convert the heading number to integers instead of numbers in words")
-            tool: str = Field(description="tool used to attack target")
-
-        # Set up a parser + inject instructions into the prompt template.
-        self.parser = PydanticOutputParser(pydantic_object=JSONObj)
-
-        self.prompt = PromptTemplate(
-            template="Answer the user query.\n{format_instructions}\n{query}\n",
-            input_variables=["query"],
-            partial_variables={"format_instructions": self.parser.get_format_instructions()},
-        )
+        # initialize the model here
+        # model_path = "petrichoro7/BERTModel"
+        # self.model = BertForQuestionAnswering.from_pretrained(model_path)
+        # self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # self.model.to(self.device)
+        self.model = pipeline("question-answering", "SauravMaheshkar/distilbert-base-cased-distilled-chaii")
+        pass
 
     def qa(self, context: str):
-        # Run prompt and model
-        prompt_and_model = self.prompt | self.model
-        output = prompt_and_model.invoke({"query": context})
-        result = self.parser.invoke(output)
-        return {"heading": result.heading, "tool": result.tool, "target": result.target}
+        heading = self.model(question="What is the heading?", context=context)
+        heading= self.convert_words_to_number(heading['answer'])
+        target = self.model(question="What is the target?", context=context)
+        target= target['answer']
+        tool = self.model(question="What is the tool to deploy?", context=context)
+        tool= tool['answer']
+        # perform NLP question-answering
+        # Tokenize the context to find the exact start and end position of the answer
+#         inputs = self.tokenizer.encode_plus("What is the heading?", context, return_tensors="pt", max_length=512, truncation=True)
+#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
+#         with torch.no_grad():
+#             outputs = self.model(**inputs)
 
-if __name__ == "__main__":
-    nlp_manager = NLPManager()
-    inputs = ["We have a blue drone coming in from a heading of zero four five. Let's get those surface-to-air missiles ready.", "There's a black helicopter on a heading of 355. Ready the laser-guided missiles."]
-    for context in inputs:
-        result = nlp_manager.qa(context)
-        print(result)
+#         heading_start = torch.argmax(outputs.start_logits, dim=1).item()
+#         heading_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+#         heading = self.tokenizer.decode(inputs["input_ids"][0][heading_start:heading_end])
+#         heading = self.convert_words_to_number(heading)
+        
+#         inputs = self.tokenizer.encode_plus("What is the tool?", context, return_tensors="pt", max_length=512, truncation=True)
+#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
+#         with torch.no_grad():
+#             outputs = self.model(**inputs)
+
+#         tool_start = torch.argmax(outputs.start_logits, dim=1).item()
+#         tool_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+#         tool = self.tokenizer.decode(inputs["input_ids"][0][tool_start:tool_end])
+#         tool = self.remove_whitespace(tool)
+        
+#         inputs = self.tokenizer.encode_plus("What is the target?", context, return_tensors="pt", max_length=512, truncation=True)
+#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
+#         with torch.no_grad():
+#             outputs = self.model(**inputs)
+
+#         target_start = torch.argmax(outputs.start_logits, dim=1).item()
+#         target_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+#         target = self.tokenizer.decode(inputs["input_ids"][0][target_start:target_end])
+#         target = self.remove_whitespace(target)
+
+        return {"heading": heading, "tool": tool, "target": target}
+        # return {"heading": '', "tool": '', "target": ''}
+
+    def convert_words_to_number(self, words):
+        word_mapping = {
+            'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+            'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+        }
+        number = ''
+        for word in words.split():
+            closest_match = process.extractOne(word, word_mapping.keys())[0]
+            number += word_mapping[closest_match]
+        return number
+
+#     def remove_whitespace(self, words):
+#         # Remove whitespace around hyphens
+#         words = words.replace(" - ", "-")
+#         return words
+    
+# if __name__ == "__main__":
+#     nlp_manager = NLPManager()
+#     inputs = "tool to deploy is big, and black nuclear bombs, target is yellow, pink pokka-dotted fisher fighter jet, heading is sevenin niner throne,."
+#     result = nlp_manager.qa(inputs)
+#     print(result)
