71fe40aacf20d57a5b9f3d54652e1781dc043652
shuyaoao <124263084+shuyaoao@users.noreply.github.com>
2024-05-30 18:29:07 +0800

    Update NLPManager.py

--- nlp/src/NLPManager.py
+++ nlp/src/NLPManager.py
@@ -1,58 +1,58 @@
 from typing import Dict
-# from transformers import BertForQuestionAnswering, AutoTokenizer, Trainer, BertTokenizer, BertForSequenceClassification
+from transformers import DistilBertForQuestionAnswering, AutoTokenizer
 import torch
 from fuzzywuzzy import process
-from transformers import pipeline
+import os
 
 class NLPManager:
     def __init__(self):
         # initialize the model here
-        # model_path = "petrichoro7/BERTModel"
-        # self.model = BertForQuestionAnswering.from_pretrained(model_path)
-        # self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # self.model.to(self.device)
-        self.model = pipeline("question-answering", "SauravMaheshkar/distilbert-base-cased-distilled-chaii")
+        model_path = "distilbert-base-cased-distilled-chaii"
+        self.model = DistilBertForQuestionAnswering.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        # self.model = pipeline("question-answering", "SauravMaheshkar/distilbert-base-cased-distilled-chaii")
         pass
 
     def qa(self, context: str):
-        heading = self.model(question="What is the heading?", context=context)
-        heading= self.convert_words_to_number(heading['answer'])
-        target = self.model(question="What is the target?", context=context)
-        target= target['answer']
-        tool = self.model(question="What is the tool to deploy?", context=context)
-        tool= tool['answer']
+        # heading = self.model(question="What is the heading?", context=context)
+        # heading= self.convert_words_to_number(heading['answer'])
+        # target = self.model(question="What is the target?", context=context)
+        # target= target['answer']
+        # tool = self.model(question="What is the tool to deploy?", context=context)
+        # tool= tool['answer']
         # perform NLP question-answering
         # Tokenize the context to find the exact start and end position of the answer
-#         inputs = self.tokenizer.encode_plus("What is the heading?", context, return_tensors="pt", max_length=512, truncation=True)
-#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
-#         with torch.no_grad():
-#             outputs = self.model(**inputs)
+        inputs = self.tokenizer.encode_plus("What is the heading?", context, return_tensors="pt", max_length=512, truncation=True)
+        inputs = {key: value.to('cuda') for key, value in inputs.items()}
+        with torch.no_grad():
+            outputs = self.model(**inputs)
 
-#         heading_start = torch.argmax(outputs.start_logits, dim=1).item()
-#         heading_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
-#         heading = self.tokenizer.decode(inputs["input_ids"][0][heading_start:heading_end])
-#         heading = self.convert_words_to_number(heading)
+        heading_start = torch.argmax(outputs.start_logits, dim=1).item()
+        heading_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+        heading = self.tokenizer.decode(inputs["input_ids"][0][heading_start:heading_end])
+        heading = self.convert_words_to_number(heading)
         
-#         inputs = self.tokenizer.encode_plus("What is the tool?", context, return_tensors="pt", max_length=512, truncation=True)
-#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
-#         with torch.no_grad():
-#             outputs = self.model(**inputs)
+        inputs = self.tokenizer.encode_plus("What is the tool?", context, return_tensors="pt", max_length=512, truncation=True)
+        inputs = {key: value.to('cuda') for key, value in inputs.items()}
+        with torch.no_grad():
+            outputs = self.model(**inputs)
 
-#         tool_start = torch.argmax(outputs.start_logits, dim=1).item()
-#         tool_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
-#         tool = self.tokenizer.decode(inputs["input_ids"][0][tool_start:tool_end])
-#         tool = self.remove_whitespace(tool)
+        tool_start = torch.argmax(outputs.start_logits, dim=1).item()
+        tool_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+        tool = self.tokenizer.decode(inputs["input_ids"][0][tool_start:tool_end])
+        tool = self.remove_whitespace(tool)
         
-#         inputs = self.tokenizer.encode_plus("What is the target?", context, return_tensors="pt", max_length=512, truncation=True)
-#         inputs = {key: value.to('cuda') for key, value in inputs.items()}
-#         with torch.no_grad():
-#             outputs = self.model(**inputs)
+        inputs = self.tokenizer.encode_plus("What is the target?", context, return_tensors="pt", max_length=512, truncation=True)
+        inputs = {key: value.to('cuda') for key, value in inputs.items()}
+        with torch.no_grad():
+            outputs = self.model(**inputs)
 
-#         target_start = torch.argmax(outputs.start_logits, dim=1).item()
-#         target_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
-#         target = self.tokenizer.decode(inputs["input_ids"][0][target_start:target_end])
-#         target = self.remove_whitespace(target)
+        target_start = torch.argmax(outputs.start_logits, dim=1).item()
+        target_end = torch.argmax(outputs.end_logits, dim=1).item() + 1
+        target = self.tokenizer.decode(inputs["input_ids"][0][target_start:target_end])
+        target = self.remove_whitespace(target)
 
         return {"heading": heading, "tool": tool, "target": target}
         # return {"heading": '', "tool": '', "target": ''}
@@ -68,13 +68,13 @@ class NLPManager:
             number += word_mapping[closest_match]
         return number
 
-#     def remove_whitespace(self, words):
-#         # Remove whitespace around hyphens
-#         words = words.replace(" - ", "-")
-#         return words
+    def remove_whitespace(self, words):
+        # Remove whitespace around hyphens
+        words = words.replace(" - ", "-")
+        return words
     
-# if __name__ == "__main__":
-#     nlp_manager = NLPManager()
-#     inputs = "tool to deploy is big, and black nuclear bombs, target is yellow, pink pokka-dotted fisher fighter jet, heading is sevenin niner throne,."
-#     result = nlp_manager.qa(inputs)
-#     print(result)
+if __name__ == "__main__":
+    nlp_manager = NLPManager()
+    inputs = "tool to deploy is big, black nuclear bombs, target is yellow, pink pokka-dotted fisher fighter jet, heading is sevenin niner throne,."
+    result = nlp_manager.qa(inputs)
+    print(result)
